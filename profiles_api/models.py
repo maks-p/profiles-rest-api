1e03a0d3d5f46d2d77de27ffb46c38205f48f94b
Maks Pazuniak <mpazuniak@gmail.com>
2020-07-04 17:19:15 -0400

    Updated Djanjo

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from djanjo.contrib.auth.models import PermissionsMixin
 
-# Create your models here.
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+  """Database model for users in the system"""
+  email = models.EmailField(max_length=255, unique=True)
+  name = models.CharField(max_length=255)
+  is_activate = models.BooleanField(default=True)
+  is_staff = models.BooleanField(default=False)
+  
